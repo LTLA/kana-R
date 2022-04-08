c11377c30a0690b38031600dc2387871615af2c0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-04-08 10:46:46 -0700

    Updated the kana library.

--- inst/include/kanaval/inputs.hpp
+++ inst/include/kanaval/inputs.hpp
@@ -139,7 +139,7 @@ inline ParamDump validate_parameters(const H5::Group& handle, bool embedded, int
             std::unordered_map<std::string, int> expected;
             expected["mtx"] = 0;
             expected["genes"] = 0;
-            expected["annotation"] = 0;
+            expected["annotations"] = 0;
 
             for (auto t : types) {
                 auto it = expected.find(t);
@@ -155,7 +155,7 @@ inline ParamDump validate_parameters(const H5::Group& handle, bool embedded, int
             if (expected["genes"] > 1) {
                 throw std::runtime_error("expected no more than one 'genes' file when format is 'MatrixMarket'");
             }
-            if (expected["annotation"] > 1) {
+            if (expected["annotations"] > 1) {
                 throw std::runtime_error("expected no more than one 'annotation' file when format is 'MatrixMarket'");
             }
             
@@ -172,11 +172,10 @@ inline ParamDump validate_parameters(const H5::Group& handle, bool embedded, int
 
     // Checking the files make sense.
     if (embedded) {
-        std::sort(bytes.begin(), bytes.end());
         hsize_t sofar = 0;
         for (const auto& b : bytes) {
             if (b.first != sofar) {
-                throw std::runtime_error("offsets and sizes of 'files' are not contiguous");
+                throw std::runtime_error("offsets and sizes of 'files' are not sorted and contiguous");
             }
             sofar += b.second;
         }
@@ -285,12 +284,20 @@ inline Details validate_results(const H5::Group& handle, const ParamDump& param_
  *     @v1_1{\[**since version 1.1**\] For multiple matrices, the constraints below apply to all files corresponding to a single matrix.}
  *     - If `format = "MatrixMarket"`, there should be exactly one `type = "mtx"` corresponding to the (possibly Gzipped) `*.mtx` file.
  *       There may be zero or one `type = "genes"`, containing a (possibly Gzipped) TSV file with the Ensembl and gene symbols for each row.
- *       There may be zero or one `type = "annotation"`, containing a (possibly Gzipped) TSV file with the annotations for each column.
+ *       There may be zero or one `type = "annotations"`, containing a (possibly Gzipped) TSV file with the annotations for each column.
  *     - If `format = "10X"` or `"H5AD"`, there should be exactly one `type = "h5"`.
  *     - For other `format`s, any `type` can be used, typically for custom resources.
  *   - `name`: a scalar string specifying the file name as it was provided to **kana**.
+ *
+ *   If `embedded = true`, we additionally expect:
  *   - `offset`: a scalar integer specifying where the file starts as an offset from the start of the remaining bytes section.
- *   - `size`: a scalar integer specifying the number of bytes in the file.
+ *     The offset for the first file should be zero, and entries in `files` should be ordered by increasing `offset`.
+ *   - `size`: a non-negative scalar integer specifying the number of bytes in the file.
+ *     The offset of each file should be equal to the sum of `size` and `offset` for the preceding file.
+ *
+ *   If `embedded = false`, we expect:
+ *   - `id`: a scalar string containing some unique identifier for this file.
+ *     The interpretation of `id` is application-specific but usually refers to some cache or database.
  *
  * @v1_1{\[**since version 1.1**\] For multiple matrices, `parameters` should also contain:}
  *
